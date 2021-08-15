ef4e81b68d325be88836dc3999e8ce74b0e0e1a3
Dmitriy <pentegov_92@mail.ru>
2021-08-15 12:36:48 +0300

    Practical work 1

---
+++ task2.js
@@ -0,0 +1,3 @@
+let name = prompt('Ввод для задачи 2: Введите любое имя: ')
+let admin = name
+document.write('Задача 2: значение name = ' + name + ' значение admin = ' + admin)
---
+++ task3.js
@@ -0,0 +1,3 @@
+let result = 1000 + '108'
+document.write(result)
+document.write(' Тип данных результата: ' + typeof (result))
